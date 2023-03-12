a8385901beed4275e26cb7d7b63ef59471f9cd46
Kevin Li <kevinli020508@gmail.com>
2023-03-12 05:09:49 -0400

    Show corpus statistics in README

---
+++ README.md
@@ -0,0 +1,10 @@
+# LIHKG Corpus Statistics
+
+| Language | Sentences | Proportion |
+| - | - | - |
+Cantonese | 46571734 | 43% |
+| Cantonese Mixed with Mandarin | 2373452 | 2% |
+| Neutral | 52694941 | 49% |
+| Mandarin | 7007148 | 6% |
+
+![sentence lengths](sentence_lengths.png)
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use dashmap::DashMap;
 use lazy_static::lazy_static;
-use rand::{thread_rng, Rng, SeedableRng};
+use plotters::prelude::*;
+use rand::{Rng, SeedableRng};
+use rayon::prelude::*;
 use regex::Regex;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{Arc, Mutex};
+use std::time::Instant;
 
 lazy_static! {
     static ref CJK_REGEX: Regex = Regex::new(r"\p{Unified_Ideograph}").unwrap();
+    static ref WORD_REGEX: Regex =
+        Regex::new(r"[[:alnum:]]+|\p{Unified_Ideograph}|\p{Punct}+").unwrap();
     static ref PUNCS: HashSet<char> = {
         SHARED_PUNCS
             .union(&ENGLISH_PUNCS)
@@ -34,29 +42,144 @@ lazy_static! {
 }
 
 fn main() {
-    // filter_processed().unwrap();
-    reservoir_sample_lines(50).unwrap();
+    filter_processed().unwrap();
+    // reservoir_sample_lines(50).unwrap();
+}
+
+fn plot_sentence_lengths(lengths: DashMap<u32, u32>) -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new("sentence_lengths.png", (640, 480)).into_drawing_area();
+
+    root.fill(&WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .x_label_area_size(60)
+        .y_label_area_size(50)
+        .margin(5)
+        .caption("Sentence Length", ("sans-serif", 50.0))
+        .build_cartesian_2d((5u32..30u32).into_segmented(), 0u32..10u32)?;
+
+    chart
+        .configure_mesh()
+        .disable_x_mesh()
+        .bold_line_style(&WHITE.mix(0.3))
+        .y_desc("Proportion")
+        .x_desc("Length")
+        .axis_desc_style(("sans-serif", 25))
+        .label_style(("sans-serif", 20))
+        .draw()?;
+
+    let total_count: u32 = lengths.iter().map(|entry| *entry.value()).sum();
+
+    chart.draw_series(
+        Histogram::vertical(&chart)
+            .style(BLUE.mix(0.5).filled())
+            .data(lengths.iter().map(|entry| {
+                (
+                    *entry.key(),
+                    (*entry.value() as f32 / total_count as f32 * 100.0).round() as u32,
+                )
+            })),
+    )?;
+
+    // To avoid the IO failure being ignored silently, we manually call the present function
+    root.present().expect("Unable to write histogram to file");
+
+    Ok(())
 }
 
 fn filter_processed() -> std::io::Result<()> {
+    let start_time = Instant::now();
+
     let dir_entries = std::fs::read_dir("processed")?;
-    let output_file = File::create("sentences.txt")?;
-    let mut writer = BufWriter::new(output_file);
-    for entry in dir_entries {
-        let file_path = entry.unwrap().path();
-        let file = File::open(&file_path).unwrap();
-        let reader = BufReader::new(file);
-        for line in reader.lines() {
-            let line = line.unwrap();
-            let (sentence, _) = split_line_at_last_tab(&line);
-            let num_cjk = count_matching_chars(sentence, &CJK_REGEX);
-            let num_total = sentence.chars().count();
-            if num_cjk >= 5 && num_cjk > ((num_total as f32 * 0.8).round() as usize) {
-                writer.write_all((filter_irrelevant_chars(sentence) + "\n").as_bytes())?;
+    let output_file = Arc::new(Mutex::new(File::create("sentences.txt")?));
+    let num_mandarin = AtomicUsize::new(0);
+    let num_mixed = AtomicUsize::new(0);
+    let num_cantonese = AtomicUsize::new(0);
+    let num_neutral = AtomicUsize::new(0);
+    let num_lines = AtomicUsize::new(0);
+    let sentence_lengths: DashMap<u32, u32> = DashMap::new();
+    dir_entries
+        .map(|entry| entry.unwrap().path())
+        .collect::<Vec<_>>()
+        .into_par_iter()
+        .for_each(|file_path| {
+            let mut sentences: Vec<String> = Vec::new();
+            let file = File::open(&file_path).unwrap();
+            let reader = BufReader::new(file);
+            for line in reader.lines() {
+                num_lines.fetch_add(1, Ordering::Relaxed);
+                let line = line.unwrap();
+                let (sentence, language) = split_line_at_last_tab(&line);
+                match language {
+                    "mandarin" => {
+                        num_mandarin.fetch_add(1, Ordering::Relaxed);
+                        continue;
+                    }
+                    "mixed" => {
+                        num_mixed.fetch_add(1, Ordering::Relaxed);
+                    }
+                    "cantonese" => {
+                        num_cantonese.fetch_add(1, Ordering::Relaxed);
+                    }
+                    "neutral" => {
+                        num_neutral.fetch_add(1, Ordering::Relaxed);
+                    }
+                    _ => panic!("Invalid language tag {}", language),
+                };
+                let num_cjk = count_matching_chars(sentence, &CJK_REGEX);
+                let num_total = sentence.chars().count();
+                if num_cjk >= 5 && num_cjk > ((num_total as f32 * 0.8).round() as usize) {
+                    let sentence = filter_irrelevant_chars(sentence);
+                    sentence_lengths
+                        .entry(count_words(&sentence).try_into().unwrap())
+                        .and_modify(|count| *count += 1)
+                        .or_insert(1);
+                    sentences.push(sentence);
+                }
             }
-        }
-    }
-    writer.flush()
+            let mut output_file = output_file.lock().unwrap();
+            for sentence in sentences {
+                output_file.write_all((sentence + "\n").as_bytes()).unwrap();
+            }
+            output_file.flush().unwrap();
+        });
+    let num_lines = num_lines.load(Ordering::Relaxed);
+    let num_cantonese = num_cantonese.load(Ordering::Relaxed);
+    let num_mixed = num_mixed.load(Ordering::Relaxed);
+    let num_neutral = num_neutral.load(Ordering::Relaxed);
+    let num_mandarin = num_mandarin.load(Ordering::Relaxed);
+    println!(
+        "{} ({:.0}%) cantonese sentences",
+        num_cantonese,
+        num_cantonese as f32 / num_lines as f32 * 100.0
+    );
+    println!(
+        "{} ({:.0}%) mixed sentences",
+        num_mixed,
+        num_mixed as f32 / num_lines as f32 * 100.0
+    );
+    println!(
+        "{} ({:.0}%) neutral sentences",
+        num_neutral,
+        num_neutral as f32 / num_lines as f32 * 100.0
+    );
+    println!(
+        "{} ({:.0}%) mandarin sentences",
+        num_mandarin,
+        num_mandarin as f32 / num_lines as f32 * 100.0
+    );
+    plot_sentence_lengths(sentence_lengths).unwrap();
+    let elapsed_time = start_time.elapsed().as_secs() / 60;
+    println!("Took: {:?} minutes", elapsed_time);
+    Ok(())
+}
+
+fn count_words(sentence: &str) -> usize {
+    // Use the regular expression to split the sentence into tokens.
+    let tokens = WORD_REGEX.find_iter(sentence);
+
+    // Count the number of tokens.
+    tokens.count()
 }
 
 fn filter_irrelevant_chars(text: &str) -> String {
