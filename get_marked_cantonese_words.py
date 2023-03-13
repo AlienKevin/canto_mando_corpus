6d5fc2cab4e61736775b5362d81f5c89f8a9eb84
Kevin Li <kevinli020508@gmail.com>
2023-03-13 07:58:54 -0400

    Add POS tags to yue_words.txt

--- get_marked_cantonese_words.py
+++ get_marked_cantonese_words.py
@@ -6,14 +6,15 @@ with open('zh_tw_words.txt', 'r') as zh_file:
 with open('yue_words.txt', 'r') as yue_file:
     yue_words = []
     for line in yue_file.read().splitlines():
-        if '\t' in line:
-            yue_words.append(line.split("\t")[0])
+        if line.endswith("\tcolloquial"):
+            yue_words.append(line.removesuffix("\tcolloquial"))
         else:
             yue_entry = []
-            for yue_word in line.split(","):
-                if not yue_word in zh_words:
-                    yue_entry.append(yue_word)
+            [variants, pos] = line.split("\t")
+            for variant in variants.split(","):
+                if not variant in zh_words:
+                    yue_entry.append(variant)
             if len(yue_entry) > 0:
-                yue_words.append(",".join(yue_entry))
+                yue_words.append(",".join(yue_entry) + "\t" + pos)
     with open('marked_yue_words.txt', 'w+') as output_file:
         output_file.writelines("%s\n" % word for word in yue_words)
