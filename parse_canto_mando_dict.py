975ed079d451f095fdd1a69caf1c298af6a143a6
Kevin Li <kevinli020508@gmail.com>
2023-03-17 22:52:51 -0400

    Print out yue words from open dict (kaifang zidian)

--- parse_canto_mando_dict.py
+++ parse_canto_mando_dict.py
@@ -14,13 +14,15 @@ def is_jyutping(word: str) -> bool:
     return re.match(r'^[a-z]+[1-6]$', word) is not None
 
 i = 0
-while i < len(dict_list):
-    word = dict_list[i]
-    i += 1
-    # skip jyutping
-    while i < len(dict_list) and is_jyutping(dict_list[i]):
+with open("canto_mando_words.txt", "w+") as mapping_file, open("open_dict_yue_words.txt", "w+") as word_file:
+    while i < len(dict_list):
+        word = dict_list[i]
         i += 1
-    if i + 1 < len(dict_list) and not is_jyutping(dict_list[i + 1]):
-        definition = dict_list[i]
-        print("{}\t{}".format(word, definition if '，' in word else definition.split('，')[0]))
-        i += 1
+        word_file.write("{}\n".format(word))
+        # skip jyutping
+        while i < len(dict_list) and is_jyutping(dict_list[i]):
+            i += 1
+        if i + 1 < len(dict_list) and not is_jyutping(dict_list[i + 1]):
+            definition = dict_list[i]
+            mapping_file.write("{}\t{}\n".format(word, definition if '，' in word else definition.split('，')[0]))
+            i += 1
